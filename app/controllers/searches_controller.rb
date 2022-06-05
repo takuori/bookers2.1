50c3aa3725d4fb00382db64cf07be9637881ebdb
takuori <104710179+takuori@users.noreply.github.com>
2022-06-05 08:06:05 +0000

    検索機能実装修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,13 @@
 class SearchesController < ApplicationController
   before_action :authenticate_user!
-  
-  def search
+
+  def search_result
     @range = params[:range]
-      if @range == "User"
-        @users = User.looks(params[:search], params[:word])
-      else
-        @books = Book.looks(params[:search], params[:word])
-      end
+    if @range == "User"
+      @users = User.looks(params[:search], params[:word])
+    else
+      @books = Book.looks(params[:search], params[:word])
+    end
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'homes/about' => 'homes#about'
-  get 'search' => 'searches#search'
+  get 'search' => 'searches#search_result'
 
   resources :users do
     resource :relationships, only: [:create, :destroy]
