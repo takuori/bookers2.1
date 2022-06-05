8454d3c19e4d255873f26685d73f51123cdc3a0c
takuori <104710179+takuori@users.noreply.github.com>
2022-06-05 05:25:31 +0000

    フォロー機能実装完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,26 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
 
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+
   validates :name, presence: true
   validates :introduction, length: { minimum: 10, maximum: 200 }
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    followings.include?(user)
+  end
 
   def get_profile_image(width, height)
     unless profile_image.attached?
