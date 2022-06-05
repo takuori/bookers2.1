9066bf5605ff932de741105ef9af34058ce96f98
takuori <104710179+takuori@users.noreply.github.com>
2022-06-05 01:02:13 +0000

    いい値機能実装完了

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about'
 
   resources :users
-  resources :books
+  resources :books do
     resource :favorites, only: [:create, :destroy]
+  end
 end
 
