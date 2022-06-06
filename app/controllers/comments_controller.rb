36f11fe34c0e64ccd23380cbab96ad130c8b6bc7
takuori <104710179+takuori@users.noreply.github.com>
2022-06-06 09:17:26 +0000

    コメント非同期実装完了

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,14 @@
 class CommentsController < ApplicationController
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.comments.new(comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to request.referer
+    @comment = current_user.comments.new(comment_params)
+    @comment.book_id = book.id
+    @comment.save
   end
 
   def destroy
-    Comment.find(params[:id]).destroy
-    redirect_to request.referer
+    @comment = Comment.find(params[:id])
+    @comment.destroy
   end
 
   private
